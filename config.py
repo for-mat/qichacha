650c3485ceb76a43e705927671b975645dc389c3
rick <546454228@qq.com>
2019-01-16 15:48:23 +0800

    增加更换token

--- config.py
+++ config.py
@@ -2,8 +2,12 @@
 """
 配置
 """
+import pymysql
 import json
-import requests
+import time
+
+db = pymysql.connect(host='192.168.1.100', port=3306, user='root', passwd='123123', db='spider_qichacha',charset='utf8')
+cursor = db.cursor()
 
 headers = {
     'User-Agent':'Mozilla/5.0 (Linux; Android 8.0.0; MHA-AL00 Build/HUAWEIMHA-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x260703FA) NetType/4G Language/zh_CN Process/appbrand2'
@@ -16,29 +20,49 @@ headers = {
     ,'Server': 'Tengine'
     ,'Set-Cookie': 'acw_tc=7ae1439b15460757517813698e31f0ddcc8ef1c06b5d1b4961e6890acf;path=/;HttpOnly;Max-Age=2678401'
 }
+def get_tokens():
 
-get_token_header = {
-'charset': 'utf-8'
-,'Accept-Encoding': 'gzip'
-,'referer': 'https://servicewechat.com/wx395200814fcd7599/26/page-frame.html'
-,'content-type': 'application/json'
-,'User-Agent' : 'Mozilla/5.0 (Linux; Android 8.0.0; MHA-AL00 Build/HUAWEIMHA-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.91 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x260703FA) NetType/WIFI Language/zh_CN Process/appbrand2'
-,'Host': 'xcx.qichacha.com'
-,'Connection': 'Keep-Alive'
-#,'code': '023SkcIA12Zfhc0xUBLA13d1IA1SkcIg'
-,'Server': 'Tengine'
-,'Content-Type': 'application/json'
-,'Access-Control-Allow-Origin': '*'
-,'Access-Control-Allow-Headers': 'X-Requested-With'
-,'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS'
-,'Timing-Allow-Origin': '*'
-}
+    cursor.execute('select wx_token from token_list where token_status=0')
+    results = cursor.fetchall()
+    tokens = []
+    for i in results:
+        wx_token = i[0]
+        tokens.append(wx_token)
+    return tokens
 
-token = 'af0dcb374ec309461ccc0cbf4cabf89e'
+tokens = get_tokens()
+token_num = 0
+tokens.reverse()
+token = tokens.pop()
 
-data = {'code': '033kq38o03ymjl1Uyoao0q918o0kq381'}
+def change_token():
+    global token_num
+    global tokens
+    global token
+    token_num += 1
+    if token_num > 800:
+        token = json.dumps(token, encoding="utf-8", ensure_ascii=False)
+        cursor.execute('update token_list set token_status=1 where wx_token=%s' % token)
+        db.commit()
+        token = tokens.pop()
+        token_num = 0
+        if len(tokens) == 0:
+            print 'need to add token~'
+            time.sleep(60)
+            tokens = get_tokens()
+            while len(tokens) ==0:
+                time.sleep(60)
+                tokens = get_tokens()
+                print 'Please add token~~'
+            else:
+                print 'OK'
 
-#js = requests.post('https://xcx.qichacha.com/wxa/v1/admin/xcxGetAccessToken',headers = get_token_header)
-#js = js.text
-#print js
-#exit()
+
+
+
+
+
+#tokens = ['9c749a0100ea9bff4e24925346e7d08e']
+
+
+
